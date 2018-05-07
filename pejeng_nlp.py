2227f56bf27c5d7d727f0a2d768a3ca870039ede
Ngakan Nyoman Gandhi <n.nyoman.gandhi@gmail.com>
2018-05-07 13:37:15 +0700

    more articles, minor modification for prints

--- pejeng_nlp.py
+++ pejeng_nlp.py
@@ -37,13 +37,17 @@ articles_no_empty_intermediate_2 = [[t for t in sublist if '\'\'' not in t]
 									for sublist in articles_no_empty_intermediate_1]
 articles_cleaned = [[t for t in sublist if t not in punctuation]
 					for sublist in articles_no_empty_intermediate_2]
+print(len(articles_cleaned))
 
 ## Simple BAG-OF-WORDS Model
 ## Looking up top 5 most-common words in the corpora
 
 # Create a counter object: counter
 counter = Counter([word for words in articles_cleaned for word in set(words)])
-print(counter.most_common(10))
+print('-----' * 8)
+print("Top 10 Words according to frequency:")
+print('-----' * 8)
+print(counter.most_common(10), '\n')
 
 ## TF-IDF Using Gensim
 # Create a gensim corpus and then apply Tfidf to that corpus
@@ -60,13 +64,23 @@ from gensim.models.tfidfmodel import TfidfModel
 
 # Create a tfidf object from corpus
 tfidf = TfidfModel(corpus)
+print('-----' * 8)
+print("TF-IDF Object from Corpus")
+print('-----' * 8)
+print(tfidf, '\n')
 
 # Checkpoint, print articles_cleaned
-print(articles_cleaned[0])
+print('-----' * 8)
+print("Cleaned Articles:")
+print('-----' * 8)
+print(articles_cleaned[0], '\n')
 
 # Check the tfidf weight in the first document of corpus
 # corpus[1] = articles_cleaned[1]
-print(tfidf[corpus[1]])
+print('-----' * 8)
+print('TF-IDF for the first document in the corpus')
+print('-----' * 8)
+print(tfidf[corpus[1]], '\n')
 
 # Test: getting the word inside a doc and its tf-idf weight
 doc = corpus[1]
@@ -76,5 +90,8 @@ tfidf_weights = tfidf[doc]
 sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True)
 
 # Print the top 5 weighted words of doc
+print('-----' * 8)
+print('Top 5 Weighted Words for corpus[1]')
+print('-----' * 8)
 for term_id, weight in sorted_tfidf_weights[:5]:
 	print(dictionary.get(term_id), weight)
